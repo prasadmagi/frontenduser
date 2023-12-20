b7fb15fa211a3bf0564ec2a694666f75fbcd0aa5
prasadmagi <magiprasad@gmail.com>
2023-12-21 00:26:30 +0530

    sendUserData Api call made

--- src/components/Private.js
+++ src/components/Private.js
@@ -1,37 +1,52 @@
 import axios from "axios";
 import React, { useContext } from "react";
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
 import UserContext from "./UserContext";
+import { popUp } from "../Helper";
 
-export const Private = (props) => {
+export const Private = () => {
   debugger;
   const [data, setdata] = useState("")
   const dataUser = useContext(UserContext)
-  const location = useLocation();
-  const handleSubmitData = ()=>{
+  const handleSubmitData = () => {
     debugger
     console.log(dataUser.username, "user");
     apicall()
   }
 
-  const apicall = async()=>{
+  const apicall = async () => {
     debugger
-    let url = window.REACT_APP_URL + "sendData";
+    let url = window.REACT_APP_URL + "sendUserData";
     let input = {
-      name: location.user,
+      name: dataUser.username,
       data: data,
     };
-    let response = await axios.post(url, input);
+    let response = await axios.put(url, input);
 
     let result = await response.data;
 
     console.log(result, "result");
+
+    if (result.msgId === -1) {
+      popUp({ message: result.message, icons: "error", title: "Error" }).then((event) => {
+        if (event.isConfirmed) {
+
+        }
+      })
+    } else {
+      popUp({ message: result.message, icons: "success", title: "Success" }).then((event) => {
+        if (event.isConfirmed) {
+
+        }
+      })
+    }
+
+    setdata("")
   }
   return (
     <div>
       <h2>Private Route with User Name : {dataUser.username} </h2>
-      <input value={data} onChange={(e)=>setdata(e.target.value)} placeholder="Add Data Here"/>
+      <input value={data} onChange={(e) => setdata(e.target.value)} placeholder="Add Data Here" />
       <button onClick={handleSubmitData}>Submit</button>
     </div>
   );
